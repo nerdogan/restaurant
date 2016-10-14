835ea2a571c18cf2ca3bd1bcde13abc5eafa2c4d
namikerdogan <erdogannamik@gmail.com>
2016-10-15 01:46:40 +0300

    saat  eklendi

--- masa1.py
+++ masa1.py
@@ -38,7 +38,7 @@ def yenile():
 
     print " "
 
-    selectt1="SELECT plu_no,urun_adi,adet,tutar,masa_no,tah_kod,kisi_sayisi,departman, islem_kod FROM YEDEK_RAPOR WHERE TARIH='"+tt1+"' and plu_no<1000 and urun_turu > 0 "
+    selectt1="SELECT plu_no,urun_adi,adet,tutar,masa_no,tah_kod,kisi_sayisi,departman, islem_kod,saat FROM YEDEK_RAPOR WHERE TARIH='"+tt1+"' and plu_no<1000 and urun_turu > 0 "
     #print selectt1
     ab=0
     aa=cur.execute(selectt1 )
@@ -46,7 +46,7 @@ def yenile():
         if row[2]<0:
             continue
 
-        curmy.execute("insert into ciro  (pluno,urun,adet,tutar,masano,tahkod,acik,tarih,kisi,departman,islem) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],"0",tt2,row[6],row[7],row[8]))
+        curmy.execute("insert into ciro  (pluno,urun,adet,tutar,masano,tahkod,acik,tarih,kisi,departman,islem,saat) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],"0",tt2,row[6],row[7],row[8],row[9]))
 
 
         ab=ab+row[3]
