523ad96fa7ce2423c9b3b634d2cd0f64f6a58954
nerdogan <erdogannamik@gmail.com>
2016-10-15 02:00:22 +0300

    departman eklendi

--- masa.py
+++ masa.py
@@ -8,7 +8,7 @@ from socket import *
 
 
 
-selectt="SELECT plu_no,urun_adi,adet,tutar,masa_no,n_05,kisi_sayisi,saat,hesap FROM DATA WHERE  plu_no<1000"
+selectt="SELECT plu_no,urun_adi,adet,tutar,masa_no,n_05,kisi_sayisi,saat,hesap,departman FROM DATA WHERE  plu_no<1000"
 
 
 
@@ -45,7 +45,7 @@ def yenile():
         if row[2]<0:
             continue
         print '%s masasinda %s TL  %s urun ' % (row[4], row[1],row[2])
-        curmy.execute("insert into ciro  (pluno,urun,adet,tutar,masano,tahkod,acik,tarih,kisi,saat,hesap) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],"1",tt2,row[6],row[7],row[8]))
+        curmy.execute("insert into ciro  (pluno,urun,adet,tutar,masano,tahkod,acik,tarih,kisi,saat,hesap,departman) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],"1",tt2,row[6],row[7],row[8],row[9]))
 
 
         ab=ab+row[3]
@@ -55,7 +55,7 @@ def yenile():
     print " "
     print " "
 
-    selectt1="SELECT plu_no,urun_adi,adet,tutar,masa_no,tah_kod,kisi_sayisi,saat FROM YEDEK_RAPOR WHERE TARIH='"+tt1+"' and plu_no<1000 and urun_turu > 0 "
+    selectt1="SELECT plu_no,urun_adi,adet,tutar,masa_no,tah_kod,kisi_sayisi,saat,departman FROM YEDEK_RAPOR WHERE TARIH='"+tt1+"' and plu_no<1000 and urun_turu > 0 "
     print selectt1
     ab=0
     aa=cur.execute(selectt1 )
@@ -67,7 +67,7 @@ def yenile():
         if row[2]<0:
             continue
         print '%s masasinda %s TL  %s urun ' % (row[4], row[1],row[2])
-        curmy.execute("insert into ciro  (pluno,urun,adet,tutar,masano,tahkod,acik,tarih,kisi,saat) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],"0",tt2,row[6],row[7]))
+        curmy.execute("insert into ciro  (pluno,urun,adet,tutar,masano,tahkod,acik,tarih,kisi,saat,departman) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(row[0],row[1],row[2],row[3],row[4],row[5],"0",tt2,row[6],row[7],row[8]))
 
 
         ab=ab+row[3]
