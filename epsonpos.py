e15e5556b52902e73d53e1f6f7760b908bf30631
nerdogan <erdogannamik@gmail.com>
2016-10-19 20:07:13 +0300

    1062 satış raporu gruplandı

--- epsonpos.py
+++ epsonpos.py
@@ -1,17 +1,19 @@
 # -*- coding: cp1254 -*-
-from escpos import *
-p = printer.Network("192.168.2.223")
+from escpos.printer import Network
+p = Network("192.168.2.223")
 
 p.image('./images/bishop.png')
 
-p.text("Big line\n")
+p.text("MUTFAK\n")
 p.text(chr(27))
 p.text(chr(45))
 p.text(chr(50))
-p.text(u'on bahce\n')
+p.text(u'DONDURUCU\n')
 p.text(chr(27))
 p.text(chr(33))
 p.text(chr(17))
-p.text('BIG TEXT\n')
-pbarcode("bishop", "CODE39",80,3,"BELOW", "A")
+p.text('NEN 2016 \n')
+p.barcode("3057", "CODE39",80,3,"BELOW", "A")
+p.text('\n')
+p.barcode("3058", "CODE39",80,3)
 p.cut()
