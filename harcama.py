50cde21cc3fece07f371c9aba68e8cc418c738db
nerdogan <erdogannamik@gmail.com>
2017-03-30 12:54:59 +0300

    3003

--- harcama.py
+++ harcama.py
@@ -20,7 +20,7 @@ def kontrol(girdi):
 
 
 wb =  xlwt.Workbook(encoding="utf-8")
-dest_filename = 'empty_bookaralik.xls'
+dest_filename = 'empty_booksubat.xls'
 date_format = xlwt.XFStyle()
 date_format.num_format_str = u'#,##0.00₺'
 date_xf = xlwt.easyxf(num_format_str='DD/MM/YYYY')
@@ -29,7 +29,7 @@ style1 = xlwt.easyxf('pattern: pattern solid, fore_colour red;')
 
 
 
-wb1 = load_workbook('C:\\Users\\NAMIK\\Google Drive\\bishop\\PERSONEL\\denizbankaralik2016.xlsx', read_only=True)
+wb1 = load_workbook('C:\\Users\\NAMIK\\Google Drive\\bishop\\PERSONEL\\denizbanksubat2017.xlsx', read_only=True)
 ws = wb1["Hesap Hareketleri"]
 aa=0
 ab=0
@@ -39,7 +39,7 @@ for row in ws.rows:
     ab=ab+1
     if ab==100000:
         break
-    if ab < 57:
+    if ab < 1:
         continue
 
     print "   "
