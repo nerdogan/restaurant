32f16cdb097085c52c8c984c005f1f01f458de24
nerdogan <erdogannamik@gmail.com>
2017-07-29 22:20:46 +0300

    20170728

--- epsonpos.py
+++ epsonpos.py
@@ -1,8 +1,10 @@
-# -*- coding: cp1254 -*-
+# -*- coding: cp1254 -*
+import modulemdb
 from escpos.printer import Network
 p = Network("192.168.2.223")
 
 p.image('./images/bishop.png')
+p.set(font='a', align='center',height=1,width=1)
 
 p.text("MUTFAK\n")
 p.text(chr(27))
@@ -13,7 +15,16 @@ p.text(chr(27))
 p.text(chr(33))
 p.text(chr(17))
 p.text('NEN 2016 \n')
-p.qr("http://nen.duckdns.org/kasa.php",size=int(3))
+p.qr("http://nen.duckdns.org/kasa.php",size=int(6))
 p.barcode("3058", "CODE39",80,3)
+p.set(font='a', align='left',height=1,width=1)
+a="01234567890123456789"
+b=a.ljust(5)
+p.text(b)
+p.text("100,00 \n")
+b=a.ljust(30)
+p.text(b)
+p.text("100,00 \n")
+p.text("012345678901234567890012345678901234567890")
 
 p.cut()
