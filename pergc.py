05b06dca762f53727efb21c0d549b94102b4a9ae
nerdogan <erdogannamik@gmail.com>
2019-11-16 19:24:56 +0300

    20191022 python 3

--- pergc.py
+++ pergc.py
@@ -1,50 +1,103 @@
-import fdb
+# -*- coding:utf8 -*-
 import MySQLdb as mdb
 from datetime import datetime,timedelta
 import time as ttim
+import smtplib
+from socket import *
+import subprocess
+import requests
+import sys
+
+url1 = 'https://pushmeapi.jagcesar.se'
+token = "1000513595:AAGoRRD_E42-JCpikul1htVbaqx5QQ-VG6c"
+ab=0
 
 
-conmy = mdb.connect("192.168.2.211", "nen","654152", "bishop",charset='utf8')
+def send_email(user, pwd, recipient, subject, body):
+
+
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print('successfully sent the mail')
+    except:
+        print("failed to send mail")
+
+
+tgtIP = gethostbyname('nen.duckdns.org')
+print(tgtIP)
+conmy = mdb.connect(tgtIP, "nen","654152", "bishop",charset='utf8',port=30000)
 curmy = conmy.cursor()
+curmy.execute("SET NAMES UTF8")
+curmy.execute("SET character_set_client=utf8")
 
 
-
-con = fdb.connect(
-    dsn='192.168.2.211:C:\DigiAccess\DigiAccess300\Data\DATABASE.GDB',
-    user='sysdba', password='masterkey',
-charset='WIN1254'
-  )
-cur=con.cursor()
-selectt="SELECT PS,AD,SOYAD FROM KIMLIK "
+selectt="SELECT adsoyad,saat,tarih,id FROM personelgc where mail='0' "
 
 
 def yenile():
 
-    ab="0"
-    aa=cur.execute(selectt)
 
-    print "ACIK MASALAR"
-    print " "
+    curmy.execute(selectt)
+    aa=curmy.fetchall()
+    if len(aa)==0:
+        print("Yeni hareket yok !!")
+
     for row in aa:
+        a1=row[0]
+        a2=str(row[1])
+        a3=str(row[2])
+        a4=(row[3])
 
-        curmy.execute("insert into personel (kod,ad,soyad,adsoyad) values (%s,%s,%s,%s)",(row[0],row[1],row[2],(row[1]+" "+row[2])))
-        print row[0],row[1],row[2]
+        bodyy="\n\n"+a3+" tarihinde saat "+a2+" personelimiz "+a1+" giriş-çıkış yapmıştır. \n\nBilgilerinize\n NAMIK ERDOĞAN"
+      # send_email('erdogannamik@gmail.com','qazxcv654152','orhangunendii@gmail.com','personel giriş çıkış bilgilendirme',bodyy)
+        appnot=a3+u" "+a2+u"  "+a1
 
-    print " "
+
+        r=requests.post(url='https://api.telegram.org/bot{0}/sendMessage'.format(token),
+                      data={'chat_id': -362841907, 'text': appnot}).json()
+        print (r)
+
+        print(row[0], row[1], row[2])
+        curmy.execute("update personelgc SET mail='1' where id=%s ",(a4,))
+        conmy.commit()
+        ttim.sleep(1)
+
+
+
+
+
+
+
+while True:
+    ab += 1
+
+    subprocess.Popen("/home/nerdogan/PycharmProjects/restaurant/venv/bin/python persatt.py",shell=True)
+
+    ttim.sleep(10)
+    yenile()
     conmy.commit()
+    a,b = divmod(ab,50)
+
+    if b==0:
+        pass
+        #subprocess.Popen('python twgonder.py')
+    print("_______________________________________________________________", ab)
+    ttim.sleep(60)
 
 
-    print " "
-    """   ab=0
-    aa=cur.execute(slectaylik )
-    print "AYLIK DAGILIM "
-    print " "
-    for row in aa:
-        print '%s -- %s  TL --  %s ADET ' % (row[0], row[2],row[1])
-        ab=ab+row[2]
 
-    print "toplam       :",ab """
-
-
-yenile()
-conmy.commit()
--- persatt.py
+++ persatt.py
@@ -1,38 +1,37 @@
 # -*- coding: utf8 -*-
+from __future__ import print_function
 __author__ = 'NAMIK'
 import sys
-reload(sys)
-sys.path.append("C:\Python27\Lib\site-packages\zklib")
+
+
+#sys.path.append("/usr/local/lib/python3.5/dist-packages/zklib")
 import subprocess
 
-import zklib1
+from zklib import zklib, zkconst
 import time
-import zkconst
+#import zkconst
 import MySQLdb as mdb
 from socket import *
 
 tgtIP = gethostbyname('nen.duckdns.org')
-print tgtIP
+print(tgtIP)
 conmy = mdb.connect(tgtIP, "nen","654152", "bishop",charset='utf8',port=30000)
 curmy = conmy.cursor()
 curmy.execute("SET NAMES UTF8")
 curmy.execute("SET character_set_client=utf8")
 
-zk = zklib1.ZKLib("192.168.2.224", 4370)
+zk = zklib.ZKLib("192.168.2.224", 4370)
 
 ret = zk.connect()
-print "Bağlantı:", ret
+print("Bağlantı:", ret)
 
 if ret == True:
-    print "Cihaz Devredışı", zk.disableDevice()
-
-
-
+    print("Cihaz Devredışı", zk.disableDevice())
 
     attendance = zk.getAttendance()
-    print len(attendance)
+    print(len(attendance))
 
-    print "Giriş çıkış listesi:"
+    print("Giriş çıkış listesi:")
 
     if ( attendance ):
         for lattendance in attendance:
@@ -43,19 +42,21 @@ if ret == True:
             else:
                 state = 'Undefined'
 
-            print " %s, %s , %s,   %s" % ( lattendance[2].date(), lattendance[2].time(), lattendance[0], state )
+            print(" %s, %s , %s,   %s" % (lattendance[2].date(), lattendance[2].time(), lattendance[0], state))
             saatt =str(lattendance[2].time())
             tarihh= str(lattendance[2].date())
-            select='INSERT INTO personelgc(enrolgc,stringgc,tarih,saat) VALUES('+lattendance[0]+',"'+tarihh+saatt+'","' + tarihh+ '","' + saatt+'") ON DUPLICATE key UPDATE saat="'+saatt+'"'
+            select = 'INSERT INTO personelgc(enrolgc,stringgc,tarih,saat) VALUES(' + lattendance[
+                0] + ',"' + tarihh + saatt + '","' + tarihh + '","' + saatt + '") ON DUPLICATE key UPDATE saat="' + saatt + '"'
 
             curmy.execute(select)
             conmy.commit()
 
-        #print "Clear Attendance:", zk.clearAttendance()
+    #print ("Clear Attendance:", zk.clearAttendance())
 
-    print "Cihaz saati:", zk.getTime()
+    print("Cihaz saati:", zk.getTime())
 
-    print "Cihaz Devrede", zk.enableDevice()
+    print("Cihaz Devrede", zk.enableDevice())
 
-    print "Bağlantı kesildi.", zk.disconnect()
+    print("Bağlantı kesildi.", zk.disconnect())
+
 #subprocess.Popen('python opencvvv1.py')
