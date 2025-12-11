27a2b040a78e461c1e1e655a992f2dde88505166
nerdogan <erdogannamik@gmail.com>
2025-12-12 00:32:57 +0300

    disable-gpu

--- saglamoglu.py
+++ saglamoglu.py
@@ -9,6 +9,10 @@ chrome_options.add_argument("user-data-dir=saglam")
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--window-size=1920,1080")
 import time
 import re
 import datetime
