2cdb3bb495eb051fe2acaa272e11a62f1a388dad
namikerdogan <erdogannamik@gmail.com>
2025-12-18 23:06:13 +0300

    geçici profil

--- saglamoglu.py
+++ saglamoglu.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
-chrome_options.add_argument("user-data-dir=saglam")
+#chrome_options.add_argument("user-data-dir=saglam")
 chrome_options.add_argument("--headless=new")
 #chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
