f0c4135c373d88d7e902421d7a3b27d8d16a51a6
nerdogan <erdogannamik@gmail.com>
2025-12-19 20:36:43 +0300

    chromedriver 143

--- saglamoglu.py
+++ saglamoglu.py
@@ -4,10 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+
 chrome_options = Options()
-#chrome_options.add_argument("user-data-dir=saglam")
+chrome_options.add_argument("user-data-dir=saglam")
 chrome_options.add_argument("--headless=new")
-#chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--window-size=1920,1080")
